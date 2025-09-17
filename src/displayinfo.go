ff1394683de98354ff4cdb07d6d9e304cc670999
pquochuy <quochuy.pham@ynov.com>
2025-09-17 09:47:06 +0200

    ajourter if dans func Display

--- src/displayinfo.go
+++ src/displayinfo.go
@@ -13,4 +13,12 @@ func DisplayInfo(c1 *Character) {
 	fmt.Println("Items de l'inventaire :", c1.Inventaire)
 	fmt.Println("Skills:", c1.Skills)
 	fmt.Println("spellbook:", c1.Skills)
+
+	if len(c1.Inventaire) == 0 {
+		fmt.Println("L'inventaire vide ")
+	} else {
+		for _, item := range c1.Inventaire {
+			fmt.Printf(" - %s   %d \n", item.Nom, item.Quantite)
+		}
+	}
 }
